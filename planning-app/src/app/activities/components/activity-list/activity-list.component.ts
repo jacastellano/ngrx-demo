91dc58245fde7c576d9e74e2de9a080c0e1e63cc
José A. Castellano <jacastellanoa@gmail.com>
2020-06-12 16:18:22 +0200

    adding dispatch

--- planning-app/src/app/activities/components/activity-list/activity-list.component.ts
+++ planning-app/src/app/activities/components/activity-list/activity-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Activity } from '../../models/activity.model';
+import { Store } from '@ngrx/store';
+import { loadActivities } from '../../store/activity.actions';
+
 
 @Component({
   selector: 'app-activity-list',
@@ -10,10 +13,13 @@ export class ActivityListComponent implements OnInit {
 
   activityList: Activity[];
 
-  constructor() { }
+  constructor(
+    private readonly store: Store,
+  ) { }
 
   ngOnInit(): void {
     this.activityList = [];
+    this.store.dispatch(loadActivities());
   }
 
 }
